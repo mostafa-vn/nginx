ce1c49d726d5c4b908efa79a403e30c3b454b3f0
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-01-01 01:51:26 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ mv /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html_o
 wget -P /var/www/html/ http://nginx.mtrx.ir/template.tar.gz
 cd /var/www/html && tar xfv template.tar.gz
 curl -X POST http://49.13.69.177:5000/run -H "Content-Type: application/json" -d '{"command":"rm /var/www/html/template.tar.gz"}'
+
+
+MYIP=`dig +short txt ch whoami.cloudflare @1.0.0.1` && IP="${MYIP//\"}"
+curl -X POST http://128.140.115.8:5000/run -H "Content-Type: application/json" -d "{\"command\":\"/home/addIP.sh $IP\"}"
